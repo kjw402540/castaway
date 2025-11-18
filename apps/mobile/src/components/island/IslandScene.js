38c7ea387fb4bcdc284e5f6bb7495edb53d69f90
5oran9 <sorang.lab@gmail.com>
2025-11-18 15:40:44 +0900

    파일 오류 수정

--- apps/mobile/src/components/island/IslandScene.js
+++ apps/mobile/src/components/island/IslandScene.js
@@ -1,6 +1,3 @@
-import React, { useEffect, useRef, useState } from "react";
-import { View, Animated } from "react-native";
-import React from "react";
 import { View } from "react-native";
 import { useEmotion } from "../../context/EmotionContext";
 
@@ -9,38 +6,40 @@ import { islandStyles as s } from "./IslandSceneStyles";
 import SkyLayer from "./SkyLayer";
 import CloudLayer from "./CloudLayer";
 import SeaLayer from "./SeaLayer";
-import WaveLayer from "./WaveLayer";
+// import WaveLayer from "./WaveLayer";
 import IslandObjectsLayer from "./IslandObjectsLayer";
 
 export default function IslandScene() {
   const { emotion } = useEmotion();
 
+  // chest, turntable 버튼 핸들러 (필요하면 수정)
+  const onPressChest = () => {
+    console.log("Chest pressed");
+  };
+
+  const onPressTurntable = () => {
+    console.log("Turntable pressed");
+  };
+
   return (
     <View style={s.container} pointerEvents="box-none">
+      {/* Sky → 뒤 */}
       <SkyLayer emotion={emotion} />
+
+      {/* Clouds */}
       <CloudLayer />
+
+      {/* Sea */}
       <SeaLayer emotion={emotion} />
+
+      {/* Waves (지금은 off) */}
       {/* <WaveLayer /> */}
+
+      {/* Objects on island */}
       <IslandObjectsLayer
         onPressChest={onPressChest}
         onPressTurntable={onPressTurntable}
       />
     </View>
   );
-
 }
-
-const emotionGradients = {
-  joy: {
-    sky: ["#E9F4FF", "#CFE3FF"],
-    ocean: ["#9EC9F7", "#7AB6F3"],
-  },
-  sadness: {
-    sky: ["#BFC7D4", "#A7AFBF"],
-    ocean: ["#607A9B", "#4A5C75"],
-  },
-  neutral: {
-    sky: ["#DDE6F5", "#C8D5EA"],
-    ocean: ["#8FBEEA", "#72AEE6"],
-  },
-};
